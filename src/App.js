9ba7370ce880ff24ac5627aa3ef8e9c8ea5060a9
yash mantri <mantriyash2025@gmail.com>
2021-05-21 12:43:54 +0530

    Github Ribbon Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
-import {AuthProvider} from './context/auth'
+import { AuthProvider } from './context/auth'
 import AuthRoute from './AuthRouter'
 
 import Home from './components/pages/Home';
@@ -15,20 +15,23 @@ import Posts from './components/pages/Posts';
 import SinglePost from './components/pages/SinglePost'
 import SingleUser from './components/pages/SingleUser';
 import followPost from './components/pages/followPost'
+import GithubRibbon from './components/GithubRibbon';
+
 function App() {
   return (
-  <AuthProvider>
-   <Router>
-     <Nav/>
-     <Route exact path='/' component={Home}/>
-     <AuthRoute exact path='/login' component={Login}/>
-     <AuthRoute exact path='/register' component={Register}/>
-     <Route exact path='/post' component={Posts}/>
-     <Route exact path='/posts/:postId' component={SinglePost}/>
-     <Route exact path='/users/:username' component={SingleUser}/>
-     <Route exact path='/follow' component={followPost}/>
-   </Router>
-   </AuthProvider>
+    <AuthProvider>
+      <Router>
+        <Nav />
+        <GithubRibbon />
+        <Route exact path='/' component={Home} />
+        <AuthRoute exact path='/login' component={Login} />
+        <AuthRoute exact path='/register' component={Register} />
+        <Route exact path='/post' component={Posts} />
+        <Route exact path='/posts/:postId' component={SinglePost} />
+        <Route exact path='/users/:username' component={SingleUser} />
+        <Route exact path='/follow' component={followPost} />
+      </Router>
+    </AuthProvider>
   );
 }
 
---
+++ src/components/GithubRibbon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GithubRibbon = () => {
+  return (
+    <a className="github-fork-ribbon left-bottom fixed" href="https://github.com/yashmantri20/MERNG-QuotesForAll-Client" target="_blank" rel="noopener noreferrer" data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
+  )
+}
+
+export default GithubRibbon
